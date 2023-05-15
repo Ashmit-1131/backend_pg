353570aaf3850d7859b2a58f6ef4232f38c3b307
Ashmit Yadav <beingashmit@gmail.com>
2023-05-16 01:21:49 +0530

    1

--- routes/clothe.routes.js
+++ routes/clothe.routes.js
@@ -8,13 +8,45 @@ clothesRouter.use(express.json());
 
 
 clothesRouter.get("/", async (req, res) => {
+  const page = req.query.page||0
+  try {
+    let count = await ClothesModel.find(req.query).countDocuments();
+    let data = await ClothesModel.find(req.query).skip(page*10).limit(10);
+    res.send({
+      message: "All products data",
+      count:count,
+      status: 1,
+      data: data,
+      error: false,
+    });
+  } catch (error) {
+    res.send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+  });
+
+  clothesRouter.get("/:id", async (req, res) => {
+    let { id: _id } = req.params;
     try {
-      const clothes = await ClothesModel.find();
-      res.send(clothes);
-    } catch (err) {
-      res.send(err.message);
+      let data = await ClothesModel.find({ _id });
+      res.send({
+        message: "All products data",
+        status: 1,
+        data: data,
+        error: false,
+      });
+    } catch (error) {
+      res.send({
+        message: "Something went wrong: " + error.message,
+        status: 0,
+        error: true,
+      });
     }
   });
+  
 
   clothesRouter.post("/add", async (req, res) => {
     const { imageURL,productName, strikedPrice ,price, itemGender, itemCategory,sale,age} = req.body;
@@ -29,6 +61,44 @@ catch(err){
   res.send({'msg':err.message})
   }
   })
+
+  clothesRouter.patch("/:id", async (req, res) => {
+    let { id: _id } = req.params;
+  
+    try {
+      await ClothesModel.findByIdAndUpdate({ _id }, req.body);
+      res.send({
+        message: "Product updated",
+        status: 1,
+        error: false,
+      });
+    } catch (error) {
+      res.send({
+        message: "Something went wrong: " + error.message,
+        status: 0,
+        error: true,
+      });
+    }
+  });
+  
+  clothesRouter.delete("/:id", async (req, res) => {
+    let { id: _id } = req.params;
+  
+    try {
+      await ClothesModel.findByIdAndDelete({ _id });
+      res.send({
+        message: "Product deleted",
+        status: 1,
+        error: false,
+      });
+    } catch (error) {
+      res.send({
+        message: "Something went wrong: " + error.message,
+        status: 0,
+        error: true,
+      });
+    }
+  });
 
   module.exports = {
     clothesRouter,
